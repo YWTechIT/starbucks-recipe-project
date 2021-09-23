58579e7784157c5ea1d173fe640f4e41c1b648ed
YWTechIT <abcd8637@naver.com>
2021-09-23 17:49:03 +0900

    ADD clock svg img, 이미지 상대경로 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,23 @@
+// main
+export { default as americano } from "./main/1-americano.jpeg";
+export { default as espresso } from "./main/2-espresso-martini.jpeg";
+export { default as caramelLatte } from "./main/3-vanilla-cream-with-caramel-latte.jpeg";
+export { default as cafeMocha } from "./main/4-cafe-mocha.jpeg";
+export { default as hazelnutCoffee } from "./main/5-ice-hazelnut-coffee.jpeg";
+export { default as cinnamonHoneyCoffee } from "./main/6-ice-cinnamon-honey-coffee.jpeg";
+export { default as mapleColdForm } from "./main/7-maple-sweet-cream-cold-form.jpeg";
+export { default as spiceOatMilkLatte } from "./main/8-spice-oat-milk-latte.jpeg";
+export { default as caramelMacchiato } from "./main/9-ice-caramel-macchiato.jpeg";
+export { default as caramelBomb } from "./main/10-caramel-bomb.jpeg";
+
+// detail
+export { default as detailAmericano } from "./detail/1-americano.jpeg";
+export { default as detailEspresso } from "./detail/2-espresso-martini.jpeg";
+export { default as detailCaramelLatte } from "./detail/3-vanila-cream-with-caramel-latte.jpeg";
+export { default as detailCafeMocha } from "./detail/4-cafe-mocha.jpeg";
+export { default as detailHazelnutCoffee } from "./detail/5-ice-hazelnut-coffee.jpeg";
+export { default as detailCinnamonHoneyCoffee } from "./detail/6-ice-cinamon-honey-coffee.jpeg";
+export { default as detailMapleColdForm } from "./detail/7-maple-sweet-cream-cold-form.jpeg";
+export { default as detailSpiceOatMilkLatte } from "./detail/8-spice-oat-milk-latte.jpeg";
+export { default as detailCaramelMacchiato } from "./detail/9-ice-caramel-machiatto.jpeg";
+export { default as detailCaramelBomb } from "./detail/10-caramel-bomb.jpeg";
