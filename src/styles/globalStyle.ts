682b3d9d90289c9704bab3f32eb4228f1b184084
YWTechIT <abcd8637@naver.com>
2021-09-23 17:49:52 +0900

    ADD globalStyle
    typhograpy를 위한 글로벌스타일 추가

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Bazier Square, Noto Sans KR, Segoe UI, Apple SD Gothic Neo, Roboto, Helvetica Neue, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    line-height: 1;
+    font-weight: 400;
+  }
+  body {
+    height: 100%;
+  }
+  button {
+    text-decoration: none;
+    cursor: pointer;
+    color: inherit;
+  }
+  a, a:hover, a:focus, a:active {
+    cursor: pointer;
+    text-decoration: none;
+    color: inherit;
+  }
+  img {
+    max-width: 100%;
+    display: block;
+  }
+  ul, li {
+  list-style: none;
+  text-decoration: none;
+  margin: 0;
+  padding: 0; 
+}
+
+  h1{
+    font-size: 48px;
+  }
+  h2{
+    font-size: 26px;
+    font-weight: bold;
+  }
+  h3{
+    font-size: 22px;
+  }
+  h4{
+    font-size: 19px;
+  }
+`;
+
+export default GlobalStyle;
