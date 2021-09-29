27d968c0bf952275e2558511174f5570879ea22a
YWTechIT <abcd8637@naver.com>
2021-09-29 16:21:26 +0900

    ADD media query, change css at detail page

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -1,29 +1,21 @@
 import { useParams } from "react-router-dom";
-import { NavBar } from "../components";
+import { Footer, NavBar } from "../components";
 import styled, { keyframes } from "styled-components";
-import { RECIPE_CONFIG } from "../config";
-import Clock from "../assets/icon/clock";
+import Recipes from "../api";
 
 interface ParamTypes {
-  id: string
+    id: string;
 }
 
 const DetailWrapper = styled.section`
-    width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin: 16px 0px;
-    padding: 1.2rem 0;
     background-color: #1f3a34;
-`
+    min-width: 300px;
+    text-align: center;
+`;
 
-const TitleWrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-`
 const fadeIn = keyframes`
   0% {
     opacity: 0
@@ -31,90 +23,122 @@ const fadeIn = keyframes`
   100% {
     opacity: 1
   }
-`
-
-const Recipe = styled.h4`
-  color: #fff;
-  margin-bottom: 10px;
-  letter-spacing: .11063rem;
-  animation-duration: 2s;
-  animation-name: ${fadeIn};
-`
+`;
 
 const Title = styled.h1`
-  color: #fff;
-  line-height: 58px;
-  letter-spacing: .11063rem;
-  animation-duration: 2s;
-  animation-name: ${fadeIn};
-  word-break: keep-all;
-`
-
-const TimeRequired = styled.section`
-    color: #FFF;
-    display: flex;
-    align-items: center;
-    padding: 0.75rem 1.25rem 1.25rem 1.25rem;
+    color: #fff;
+    line-height: 6.4rem;
+    letter-spacing: 0.11063rem;
     animation-duration: 2s;
     animation-name: ${fadeIn};
-`
+    word-break: keep-all;
+    padding: 1.4rem;
+`;
 
 const ImgStyle = styled.img`
     width: 100%;
-    height: 356px;
-    object-fit: cover;
-    object-position: center;
-    margin-bottom: 24px;
+    animation-duration: 2s;
+    animation-name: ${fadeIn};
+
+    @media screen and (min-width: 720px) {
+        width: 70%;
+    }
+
+    @media screen and (min-width: 1024px) {
+        width: 50%;
+    }
 `;
 
-const IngredientWrapper = styled.section`
-  display: flex;
-  padding: 3.125rem 0;
+const OrderWrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+    padding: 3.125rem 0;
+`;
+
+const OrderTitle = styled.h4`
+    color: #fff;
+    font-size: 26px;
+    letter-spacing: 0.11063rem;
+    margin-bottom: .4rem;
+    font-weight: 700;
+
+    @media screen and (min-width: 720px) {
+        font-size: 32px;
+    }
+
+    @media screen and (min-width: 1024px) {
+        font-size: 36px;
+    }
+`;
+
+const OrderContents = styled.section`
+    display: flex;
+    flex-direction: column;
+    margin: 22px;
+    padding: 0 1rem;
+    word-break: keep-all;
+`;
+
+const BulletStyle = styled.li`
+    display: list-item;
+    list-style-type: circle;
+    color: #00754a;
+    font-size: 18px;
+    align-self: baseline;
+    padding-inline-start: 1.6rem;
+
+    @media screen and (min-width: 720px) {
+        font-size: 24px;
+    }
+
+    @media screen and (min-width: 1024px) {
+        font-size: 28px;
+    }
 `
 
-const IngredientTitle = styled.h4`
-  color: #fff;
-  letter-spacing: .11063rem;
-`
+const OrderContent = styled.span`
+    font-size: 18px;
+    color: #fff;
+    font-weight: 400;
+    letter-spacing: 0.14em;
+    line-height: 28px;
+    word-break: keep-all;
 
-const StepWrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 5rem 0;
-`
+    @media screen and (min-width: 720px) {
+        font-size: 20px;
+        line-height: 30px;
+    }
 
-const StepTitle = styled.h4`
-  color: #006c49;
-  letter-spacing: .11063rem;
-  font-weight: 600;
-`
+    @media screen and (min-width: 1024px) {
+        font-size: 24px;
+        line-height: 36px;
+    }
+`;
 
 const Detail = () => {
-  const {id} = useParams<ParamTypes>();
-  const detailSrc = RECIPE_CONFIG[+(id)-1].detailSrc
-  
-  return(
-    <>
-    <NavBar />
-    <DetailWrapper>
-      <TitleWrapper>
-      <Recipe>레시피</Recipe>
-      <Title>{RECIPE_CONFIG[+(id)-1].title}</Title>
-      <TimeRequired><Clock size={20} color="#FFF"/>{RECIPE_CONFIG[+(id)-1].required} mins</TimeRequired>
-      </TitleWrapper>
-      <ImgStyle src={detailSrc[RECIPE_CONFIG[+(id)-1].detailAlt]}/>
-      <IngredientWrapper>
-        <IngredientTitle>Ingredient</IngredientTitle>
-      </IngredientWrapper>
-    </DetailWrapper>
-    <StepWrapper>
-      <StepTitle>만드는 방법</StepTitle>
-      {/* {RECIPE_CONFIG[+(id)-1].step.map((item) => (item))} */}
+    const { id } = useParams<ParamTypes>();
+    const recipe = Recipes.getRecipe(id)[0];
 
-    </StepWrapper>
-    </>
-  )
-}
+    return (
+        <>
+            <NavBar />
+            <DetailWrapper>
+                <Title>{recipe.title}</Title>
+                <ImgStyle src={recipe.src[recipe.alt]} />
+                <OrderWrapper>
+                    <OrderTitle>사이렌 오더</OrderTitle>
+                    <OrderContents>
+                        {recipe.step.map((item, idx) => (
+                            <>
+                            <BulletStyle><OrderContent key={idx}>{item}</OrderContent></BulletStyle>
+                            </>
+                        ))}
+                    </OrderContents>
+                </OrderWrapper>
+            </DetailWrapper>
+            <Footer />
+        </>
+    );
+};
 
 export default Detail;
