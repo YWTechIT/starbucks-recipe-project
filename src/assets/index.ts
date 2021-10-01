aad79a5f6523684e513f7945c800f98f925680b6
YWTechIT <abcd8637@naver.com>
2021-10-01 16:15:36 +0900

    Delete unUse detail image

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,5 @@
 // main
 export { default as captainCrunchBerry } from "./main/01-captain-crunch-berry.jpeg";
-
 export { default as shrek } from "./main/02-shrek.jpeg";
 export { default as pigbar } from "./main/03-pigbar.jpeg";
 export { default as twix } from "./main/04-twix.jpeg";
@@ -14,16 +13,3 @@ export { default as hotHunt } from "./main/11-hot-hunt.jpeg";
 export { default as kuAndKu } from "./main/12-ku-and-ku.jpeg";
 export { default as dolcheLatte } from "./main/13-dolche-latte.jpeg";
 export { default as hwiYoung } from "./main/14-hwi-young.jpeg";
-
-
-// detail
-export { default as detailAmericano } from "./detail/1-americano.jpeg";
-export { default as detailEspresso } from "./detail/2-espresso-martini.jpeg";
-export { default as detailCaramelLatte } from "./detail/3-vanila-cream-with-caramel-latte.jpeg";
-export { default as detailCafeMocha } from "./detail/4-cafe-mocha.jpeg";
-export { default as detailHazelnutCoffee } from "./detail/5-ice-hazelnut-coffee.jpeg";
-export { default as detailCinnamonHoneyCoffee } from "./detail/6-ice-cinamon-honey-coffee.jpeg";
-export { default as detailMapleColdForm } from "./detail/7-maple-sweet-cream-cold-form.jpeg";
-export { default as detailSpiceOatMilkLatte } from "./detail/8-spice-oat-milk-latte.jpeg";
-export { default as detailCaramelMacchiato } from "./detail/9-ice-caramel-machiatto.jpeg";
-export { default as detailCaramelBomb } from "./detail/10-caramel-bomb.jpeg";
