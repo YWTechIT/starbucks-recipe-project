5e748680fe8af3df02e0a184cf101cbf5ac91456
YWTechIT <abcd8637@naver.com>
2021-10-26 08:00:03 +0900

    ADD 좋아요 로컬스토리지 저장 기능
    좋아요 버튼을 누르고 새로고침을 해도 좋아요 버튼을 누른 레시피는 좋아요가 되어있다.

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,26 +1,65 @@
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { RecipeType } from "../../types";
-import { CardWrapper, ImgStyle, ContentWrapper, Title, Desc, Type, Button, ImgWrapper } from "./style";
+import Like from "../like";
+import {
+    CardWrapper,
+    ImgStyle,
+    ContentWrapper,
+    Title,
+    Desc,
+    Type,
+    Button,
+    ImgWrapper,
+    HashTag,
+} from "./style";
 
 interface CardProps {
     item: RecipeType;
+    bookMark: string[];
+    handleBookMark: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
-const Card = ({ item }: CardProps) => {
+const onSaved = true;
+const offSaved = false;
+
+const Card = ({ item, bookMark, handleBookMark }: CardProps) => {
     let history = useHistory();
     const beverageType = item.type.split("_").join(" ");
+    const [liked, setLiked] = useState<boolean>(bookMark.includes(item.id) ? onSaved : offSaved);
+
+    const handleSaved = () => {
+        if (liked) {
+            handleBookMark((bookMarks) => bookMarks.filter((bookMark) => bookMark !== item.id));
+        } else {
+            handleBookMark((bookMarks) => [...bookMarks, item.id]);
+        }
+        setLiked((liked) => !liked);
+    };
 
     return (
         <CardWrapper>
             <ImgWrapper>
-                <ImgStyle src={item.src} alt={item.title} onClick={() => history.push(`/recipe/${item.id}`)}/>
+                <ImgStyle
+                    src={item.src}
+                    alt={item.title}
+                    onClick={() => history.push(`/recipe/${item.id}`)}
+                />
             </ImgWrapper>
             <ContentWrapper>
                 <Title>{item.title}</Title>
-                <Desc>{item.desc}</Desc>
+                <HashTag>
+                    {item.tags.map((item, idx) => (
+                        <span key={idx}>{`#${item} `}</span>
+                    ))}
+                </HashTag>
+                <Desc fontSize="18">{item.desc}</Desc>
                 <Type>{beverageType}</Type>
                 <Type>₩ {item.price.toLocaleString()}</Type>
-                <Button onClick={() => history.push(`/recipe/${item.id}`)}>GET THE RECIPE</Button>
+                <Button onClick={() => history.push(`/recipe/${item.id}`)}>
+                    GET THE RECIPE
+                </Button>
+                <Like liked={liked} handleLiked={handleSaved} item={item}/>
             </ContentWrapper>
         </CardWrapper>
     );
