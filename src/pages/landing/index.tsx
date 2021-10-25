599f70d3ef4b81984529e4726a5c8ee51e159c46
YWTechIT <abcd8637@naver.com>
2021-10-26 08:01:04 +0900

    ADD 좋아요기능, 정렬기능

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,69 +1,64 @@
 import { useCallback, useState } from "react";
-import { Card, Container, Footer, NavBar } from "../../components";
-import { RECIPE_SAMPLE } from "../../fixture";
-import { RecipeType } from "../../types";
-import { Button, ButtonWrapper, FilterContainer, FilterWrapper } from "./style";
-import Comment from "../../components/comment";
-
-enum SortOrder {
-    asc = "asc",
-    desc = "desc"
-}
-
-enum SortBy {
-    price = "price",
-    name = "name",
-}
-
-enum PriceFilter {
-    asc = "가격 높은 순",
-    desc = "가격 낮은 순"
-}
-
-enum NameFilter {
-    asc = "이름 오름차 순",
-    desc = "이름 내림차 순"
-}
+import { Card, Container, Footer, NavBar, Comment, FilterBar } from "../../components";
+import { RECIPE_SAMPLE } from "../../fixture/recipe";
+import { FilterType, RecipeType } from "../../types";
+import { CardsContainer } from "../../components/card/style";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
 
 const Landing = () => {
-    const [priceSort, setPriceSort] = useState<SortOrder>(SortOrder.asc);
-    const [nameSort, setNameSort] = useState<SortOrder>(SortOrder.asc);
     const [filteredData, setFilteredData] = useState<RecipeType[]>(RECIPE_SAMPLE);
-        
-    const getSortByPrice = useCallback((sortBy: SortBy) => {
-        if (sortBy === SortBy.price){
-            const sortByPrice = priceSort === SortOrder.asc ? [...RECIPE_SAMPLE].sort((a, b) => a.price - b.price) : [...RECIPE_SAMPLE].sort((a, b) => b.price - a.price);
-            const togglePriceSortOrder = priceSort === SortOrder.asc ? SortOrder.desc : SortOrder.asc;
-            setFilteredData(sortByPrice);
-            setPriceSort(togglePriceSortOrder);
-        }else{
-            const sortByName = nameSort === SortOrder.asc ? [...RECIPE_SAMPLE].sort((a, b) => a.title < b.title ? -1 : 1) : [...RECIPE_SAMPLE].sort((a, b) => a.title > b.title ? -1 : 1)
-            const toggleNameSortOrder = nameSort === SortOrder.asc ? SortOrder.desc : SortOrder.asc;
-            setFilteredData(sortByName);
-            setNameSort(toggleNameSortOrder);
-        }
-    }, [priceSort, nameSort])
+    const [bookMark, setBookMark] = useLocalStorage<string[]>("likes", []);
+
+    const getSortByFilter = useCallback(
+        (type: FilterType) => {
+            let sortByTypeData: RecipeType[];
+            if (type === FilterType.nameAsc)
+                sortByTypeData = [...filteredData].sort((a, b) =>
+                    a.title < b.title ? -1 : a.title > b.title ? 1 : 0
+                );
+            else if (type === FilterType.nameDesc)
+                sortByTypeData = [...filteredData].sort((a, b) =>
+                    a.title > b.title ? -1 : a.title < b.title ? 1 : 0
+                );
+            else if (type === FilterType.priceHigh)
+                sortByTypeData = [...filteredData].sort(
+                    (a, b) => b.price - a.price
+                );
+            else if (type === FilterType.priceLow)
+                sortByTypeData = [...filteredData].sort(
+                    (a, b) => a.price - b.price
+                );
+            else if (type === FilterType.likeHigh)
+                sortByTypeData = [...filteredData].sort(
+                    (a, b) => b.likes - a.likes
+                );
+            else if (type === FilterType.likeLow)
+                sortByTypeData = [...filteredData].sort(
+                    (a, b) => a.likes - b.likes
+                )
+            else sortByTypeData = [...RECIPE_SAMPLE];
+            setFilteredData(sortByTypeData);
+        },
+        [filteredData]
+    );
 
     return (
         <>
             <NavBar />
-            <FilterContainer>
-                <div>전체 {RECIPE_SAMPLE.length}개</div>
-                <FilterWrapper>
-                    <ButtonWrapper>
-                        <Button borderRight={true} onClick={() => getSortByPrice(SortBy.price)}>{priceSort === SortOrder.asc ? PriceFilter.desc : PriceFilter.asc}</Button>
-                    </ButtonWrapper>
-                    <ButtonWrapper>
-                        <Button borderRight={false} onClick={() => getSortByPrice(SortBy.name)}>{nameSort === SortOrder.asc ? NameFilter.asc : NameFilter.desc}</Button>
-                    </ButtonWrapper>
-                </FilterWrapper>
-            </FilterContainer>
             <Container>
-                {filteredData.map((item) => (
-                    <Card item={item} key={item.id}></Card>
-                ))}
+                <FilterBar getSortByFilter={getSortByFilter} />
+                <CardsContainer>
+                    {filteredData.map((item) => (
+                        <Card
+                            item={item}
+                            key={item.id}
+                            bookMark={bookMark}
+                            handleBookMark={setBookMark}
+                        />
+                    ))}
+                </CardsContainer>
+                <Comment />
             </Container>
-            <Comment />
             <Footer />
         </>
     );
