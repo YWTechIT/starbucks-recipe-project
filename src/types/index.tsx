81503466e78e0448b023ae99873b9f15f0d72901
YWTechIT <abcd8637@naver.com>
2021-10-26 07:55:25 +0900

    ADD enum filterType, filterFixtureType

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -6,9 +6,27 @@ export interface RecipeType {
     type: BeverageType;
     step: string[];
     price: number;
+    tags: string[];
+    likes: number;
 }
 
 export enum BeverageType {
     ICED = "ICED_BEVERAGES",
-    HOT = "HOT_BEVERAGES"
+    HOT = "HOT_BEVERAGES",
+}
+
+export enum FilterType {
+    nameAsc = "이름 오름차순",
+    nameDesc = "이름 내림차순",
+    priceHigh = "높은가격순",
+    priceLow = "낮은가격순",
+    popularity = "인기순",
+    likeHigh = "좋아요높은순",
+    likeLow = "좋아요낮은순",
+}
+
+export interface FilterFixtureType {
+    id: string;
+    title: string;
+    filter: FilterType;
 }
