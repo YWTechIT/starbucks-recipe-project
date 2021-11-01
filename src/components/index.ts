baca423d67acafc34634c2d2c8fcf6bd9cd138b9
YWTechIT <abcd8637@naver.com>
2021-11-01 18:25:56 +0900

    ADD 상대경로

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@
 export { default as Card } from "./card";
 export { default as Container } from "./container";
 export { default as Footer } from "./footer";
-export { default as NavBar } from "./navbar";
+export { default as Header } from "./header";
 export { default as Filter } from "./filter";
 export { default as Comment } from "./comment";
 export { default as Modal } from "./modal";
