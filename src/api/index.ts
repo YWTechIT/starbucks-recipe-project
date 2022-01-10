ef32db9df0b25288945f4005316749a4ef19b009
YWTechIT <abcd8637@naver.com>
2022-01-10 16:15:11 +0900

    Fix 구조분해할당

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,12 +2,11 @@ import { RECIPE_SAMPLE } from "../fixture/recipe";
 import { RecipeType } from "../types";
 
 interface Methods {
-  getRecipe: (targetId: string) => void;
+    getRecipe: (targetId: string) => void;
 }
 
-export class Recipes implements Methods{
-
-  getRecipe = (targetId: string): RecipeType[] => {
-    return RECIPE_SAMPLE.filter((ele) => ele.id === targetId);
-  }
+export class Recipes implements Methods {
+    getRecipe = (targetId: string): RecipeType[] => {
+        return RECIPE_SAMPLE.filter((ele) => ele.id === targetId);
+    };
 }
