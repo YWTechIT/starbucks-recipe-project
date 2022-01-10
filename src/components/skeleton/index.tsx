0bcec315a71a504fdfec125503640ef577723619
YWTechIT <abcd8637@naver.com>
2022-01-10 16:25:48 +0900

    Fix Skeleton size

--- src/components/skeleton/index.tsx
+++ src/components/skeleton/index.tsx
@@ -3,12 +3,7 @@ import { SkeletonWrapper } from "./style";
 
 const Skeleton = (props: any) => (
     <SkeletonWrapper>
-        <ContentLoader
-            viewBox="0 0 900 507"
-            height={600}
-            width="100%"
-            {...props}
-        >
+        <ContentLoader viewBox="0 0 900 507" {...props}>
             {/* first row */}
             <rect x="70" y="60" rx="0" ry="0" width="230" height="140" />
             <rect x="70" y="209" rx="0" ry="0" width="230" height="15" />
